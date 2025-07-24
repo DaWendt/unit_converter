64f50496347ab060c7dd77ffb7f8937a35dba520
Daniel Wendt <daniel.wendt@wendt-basis.de>
2025-07-24 11:54:56 +0200

    fix the cmake

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/danielwendt/Projects/c++/unit_converter
+# Build directory: /Users/danielwendt/Projects/c++/unit_converter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/googletest-build")
+subdirs("tests")
